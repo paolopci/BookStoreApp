04addbfc59d53f71df2b25c02f660d5c7d2b5cff
Paolo Paci <paolopci@yahoo.it>
2025-05-17 15:00:57 +0200

    install packages Serilog and Serilog.Expression

--- BookStoreApp.API/Program.cs
+++ BookStoreApp.API/Program.cs
@@ -1,6 +1,13 @@
+using Serilog;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Host.UseSerilog();
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
