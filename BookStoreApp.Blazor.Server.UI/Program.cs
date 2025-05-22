b5ff06aea1533fcc24d470b7c0bc5729b36307df
Paolo Paci <paolopci@yahoo.it>
2025-05-22 12:28:47 +0200

    update AuthenticationService

--- BookStoreApp.Blazor.Server.UI/Program.cs
+++ BookStoreApp.Blazor.Server.UI/Program.cs
@@ -1,5 +1,6 @@
 
 using Blazored.LocalStorage;
+using BookStoreApp.Blazor.Server.UI.Services.Authentication;
 using BookStoreApp.Blazor.Server.UI.Services.Base;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
@@ -10,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 
 builder.Services.AddHttpClient<IClient,Client>("Default", client =>
 {
--- BookStoreApp.Blazor.Server.UI/Services/Authentication/AuthenticationService.cs
+++ BookStoreApp.Blazor.Server.UI/Services/Authentication/AuthenticationService.cs
@@ -1,21 +1,41 @@
-﻿using BookStoreApp.Blazor.Server.UI.Services.Base;
+﻿using Blazored.LocalStorage;
+using BookStoreApp.Blazor.Server.UI.Services.Base;
 
 
 namespace BookStoreApp.Blazor.Server.UI.Services.Authentication
 {
     public class AuthenticationService : IAuthenticationService
     {
-        private readonly IClient httpClient;
+        private readonly IClient _httpClient;
+        private readonly ILocalStorageService _localStorage;
+        private const string TokenKey = "authToken";
 
-        public AuthenticationService(IClient httpClient)
+        public AuthenticationService(IClient httpClient, ILocalStorageService localStorage)
         {
-            this.httpClient = httpClient;
+            _httpClient = httpClient;
+            _localStorage = localStorage;
         }
 
         public async Task<bool> AuthenticationAsync(LoginUserDto loginModel, CancellationTokenSource token)
         {
-            var response = await httpClient.LoginAsync(loginModel, token.Token);
-            return response != null && !string.IsNullOrEmpty(response.Token);
+            var response = await _httpClient.LoginAsync(loginModel, token.Token);
+
+            if (response != null && !string.IsNullOrEmpty(response.Token))
+            {
+                // Store the JWT token in local storage
+                await _localStorage.SetItemAsync(TokenKey, response.Token);
+                return true;
+            }
+            return false;
+        }
+
+
+
+
+        public async Task LogOut()
+        {
+            // Remove the JWT token from local storage
+            await _localStorage.RemoveItemAsync(TokenKey);
         }
     }
 }
