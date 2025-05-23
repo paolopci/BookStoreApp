8c9bb903763cf84acc4353d1afbba8a08ef56b1a
Paolo Paci <paolopci@yahoo.it>
2025-05-23 16:47:02 +0200

    Step 2: complete AuthorService.cs

--- BookStoreApp.Blazor.Server.UI/Program.cs
+++ BookStoreApp.Blazor.Server.UI/Program.cs
@@ -1,11 +1,10 @@
 
 using Blazored.LocalStorage;
 using BookStoreApp.Blazor.Server.UI.Providers;
+using BookStoreApp.Blazor.Server.UI.Services;
 using BookStoreApp.Blazor.Server.UI.Services.Authentication;
 using BookStoreApp.Blazor.Server.UI.Services.Base;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +13,8 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
-builder.Services.AddScoped<ApiAuthenticationStateProvider>();  
+builder.Services.AddScoped<IAuthorService, AuthorService>();
+builder.Services.AddScoped<ApiAuthenticationStateProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider>(p =>
                                                            p.GetRequiredService<ApiAuthenticationStateProvider>());
 
---
+++ BookStoreApp.Blazor.Server.UI/Services/AuthorService.cs
@@ -0,0 +1,41 @@
+﻿using Blazored.LocalStorage;
+using BookStoreApp.Blazor.Server.UI.Services.Base;
+
+
+namespace BookStoreApp.Blazor.Server.UI.Services
+{
+    public class AuthorService : BaseHttpService, IAuthorService
+    {
+        private readonly IClient _client;
+        public AuthorService(IClient client, ILocalStorageService localStorage) : base(client, localStorage)
+        {
+            _client = client;
+        }
+
+        
+        public async Task<Response<List<AuthorReadOnlyDto>>> GetAllAuthors()
+        {
+            Response<List<AuthorReadOnlyDto>> response;
+
+            try
+            {
+                // Recupera e imposta il token Bearer per l'autenticazione delle richieste HTTP.
+                await GetBearerToken();
+               
+                var data = await _client.AuthorsAllAsync();
+                response = new Response<List<AuthorReadOnlyDto>>()
+                {
+                    Data = data.ToList(),
+                    Success = true
+                };
+            }
+            catch (ApiException apiException)
+            {
+                // Converte l'eccezione API in una risposta standardizzata.
+                response = ConvertApiException<List<AuthorReadOnlyDto>>(apiException);
+            }
+
+            return response;
+        }
+    }
+}
