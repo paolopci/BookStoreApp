b5b9423df90333df3bc4abd1e4ce074993250ae9
Paolo Paci <paolopci@yahoo.it>
2025-05-25 10:55:23 +0200

    add AuthorDetailsDto in Api update mapper

---
+++ BookStoreApp.Blazor.Server.UI/Configuration/MapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+
+namespace BookStoreApp.Blazor.Server.UI.Configuration
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            // Add your mapping configurations here
+           
+           //  CreateMap<Author, DestinationModel>();
+            // CreateMap<AuthorReadOnlyDto, AuthorViewModel>();
+        }
+    }
+}
