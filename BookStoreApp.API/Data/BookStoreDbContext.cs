fb7dfd7ad56f7e618a7f0fff03712da1257aaa90
Paolo Paci <paolopci@yahoo.it>
2025-05-26 15:32:03 +0200

    complete create new Book with save image

--- BookStoreApp.API/Data/BookStoreDbContext.cs
+++ BookStoreApp.API/Data/BookStoreDbContext.cs
@@ -46,7 +46,7 @@ public partial class BookStoreDbContext : IdentityDbContext<ApiUser>
 
             entity.HasIndex(e => e.Isbn, "UQ__Books__447D36EA7C84AE11").IsUnique();
 
-            entity.Property(e => e.Image).HasMaxLength(50);
+            entity.Property(e => e.Image).HasMaxLength(250);
             entity.Property(e => e.Isbn)
                 .HasMaxLength(50)
                 .HasColumnName("ISBN");
--- BookStoreApp.API/Program.cs
+++ BookStoreApp.API/Program.cs
@@ -77,7 +77,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.UseCors("AllowAll");
 
 
