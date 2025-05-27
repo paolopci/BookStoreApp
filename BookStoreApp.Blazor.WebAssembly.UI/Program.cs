173602f74d5b21eeae7376314ba615410baa5d6c
Paolo Paci <paolopci@yahoo.it>
2025-05-27 11:38:31 +0200

    Step1 : import files from Blazor.Server --> blazor.WebAssembly

--- BookStoreApp.Blazor.WebAssembly.UI/Program.cs
+++ BookStoreApp.Blazor.WebAssembly.UI/Program.cs
@@ -1,4 +1,6 @@
+using Blazored.LocalStorage;
 using BookStoreApp.Blazor.WebAssembly.UI;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +9,23 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddScoped<IAuthorService, AuthorService>();
+builder.Services.AddScoped<IBookService, BookService>();
+
+builder.Services.AddScoped<ApiAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(p =>
+                                                           p.GetRequiredService<ApiAuthenticationStateProvider>());
+
+builder.Services.AddHttpClient<IClient, Client>("Default", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7073/");
+});
+builder.Services.AddAutoMapper(typeof(MapperConfig));
+
+
+
 
 await builder.Build().RunAsync();
