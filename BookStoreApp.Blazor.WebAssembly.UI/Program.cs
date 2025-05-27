f46da2eef84c7079a41704201f6ceadc51502c5d
Paolo Paci <paolopci@yahoo.it>
2025-05-27 14:30:49 +0200

    add <CascadingAuthenticationState>

--- BookStoreApp.Blazor.WebAssembly.UI/Program.cs
+++ BookStoreApp.Blazor.WebAssembly.UI/Program.cs
@@ -4,7 +4,7 @@ using BookStoreApp.Blazor.WebAssembly.UI.Configuration;
 using BookStoreApp.Blazor.WebAssembly.UI.Providers;
 using BookStoreApp.Blazor.WebAssembly.UI.Services;
 using BookStoreApp.Blazor.WebAssembly.UI.Services.Authentication;
-
+using BookStoreApp.Blazor.WebAssembly.UI.Services.Base;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -19,6 +19,7 @@ builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 builder.Services.AddScoped<IAuthorService, AuthorService>();
 builder.Services.AddScoped<IBookService, BookService>();
+builder.Services.AddScoped<IClient, Client>();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<ApiAuthenticationStateProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider>(p =>
