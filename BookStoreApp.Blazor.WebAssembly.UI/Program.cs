3c995d20a5603dbf9b4b36d8cc5e9ecf9122afb4
Paolo Paci <paolopci@yahoo.it>
2025-05-27 14:25:07 +0200

    builder.Services.AddAuthorizationCore();

--- BookStoreApp.Blazor.WebAssembly.UI/Program.cs
+++ BookStoreApp.Blazor.WebAssembly.UI/Program.cs
@@ -4,7 +4,7 @@ using BookStoreApp.Blazor.WebAssembly.UI.Configuration;
 using BookStoreApp.Blazor.WebAssembly.UI.Providers;
 using BookStoreApp.Blazor.WebAssembly.UI.Services;
 using BookStoreApp.Blazor.WebAssembly.UI.Services.Authentication;
-using BookStoreApp.Blazor.WebAssembly.UI.Services.Base;
+
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -19,16 +19,12 @@ builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 builder.Services.AddScoped<IAuthorService, AuthorService>();
 builder.Services.AddScoped<IBookService, BookService>();
-
+builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<ApiAuthenticationStateProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider>(p =>
-                                                           p.GetRequiredService<ApiAuthenticationStateProvider>());
-
-
+                                           p.GetRequiredService<ApiAuthenticationStateProvider>());
 
 builder.Services.AddScoped(sp => new HttpClient() {BaseAddress = new Uri("https://localhost:7073/")});
-
-
 builder.Services.AddAutoMapper(typeof(MapperConfig));
 
 
